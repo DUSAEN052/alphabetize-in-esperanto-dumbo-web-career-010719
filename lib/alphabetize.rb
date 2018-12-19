9f884fa14206ebc9c795035dbd9ab96dd24a9bd1
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-19 09:14:34 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,15 +1,15 @@
 def alphabetize(arr)
   # code here
   e_a = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  output = []
   
   arr.each do |str|
-    puts str
-    str.split("").map do |letter|
-      e_a.index(letter)
+    nums = []
+    str.split("").each do |letter|
+      nums << e_a.index(letter)
     end
+    output << nums
   end
   
-  "ĉu vi parolas esperanton".split("").map do |letter|
-    puts e_a.index(letter)
-  end
+  output
 end
