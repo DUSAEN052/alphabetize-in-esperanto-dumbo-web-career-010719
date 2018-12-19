8f73535fd66888ac016242a45c4817213cd1c2c3
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-19 09:16:34 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,15 +1,10 @@
 def alphabetize(arr)
   # code here
   e_a = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  output = []
   
   arr.each do |str|
-    nums = []
-    str.split("").each do |letter|
-      nums << e_a.index(letter)
+    str.split("").map do |letter|
+      e_a.index(letter)
     end
-    output << nums
   end
-  
-  output
 end
