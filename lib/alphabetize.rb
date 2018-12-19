c38c1eb9c9b64229d3a9b191755a11d6c3564b93
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-19 08:58:22 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,6 +3,8 @@ def alphabetize(arr)
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   
   arr.sort_by do |str|
-    
-  
+    str.split(" ").map do |letter|
+      ESPERANTO_ALPHABET.index(letter)
+    end
+  end
 end
